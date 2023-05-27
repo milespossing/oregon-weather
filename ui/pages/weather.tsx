96bddb904fe51f4ad21e394181750a7e2ed5361e
Miles Possing <7330275+milespossing@users.noreply.github.com>
2023-05-27 13:21:37 -0700

    Adding docker compose
    Fixing UI

--- ui/pages/weather.tsx
+++ ui/pages/weather.tsx
@@ -1,5 +1,5 @@
 import { GetServerSideProps } from 'next'
-import { WeatherReport, WeatherReportSchema } from '../schemas/weather' // adjust the path to your schema accordingly
+import { WeatherReport, WeatherReportSchema } from '@/schemas/weather' // adjust the path to your schema accordingly
 import WeatherChart from '../components/weather'
 import * as fs from "fs"; // adjust the path to your component accordingly
 
@@ -10,6 +10,19 @@ type WeatherPageProps = {
 const WeatherPage = ({ weatherReport }: WeatherPageProps) => {
   return <WeatherChart data={weatherReport} />
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const data = fs.readFileSync(process.env.WEATHER_FILE!).toString();
+
+  const weatherReport = WeatherReportSchema.parse(JSON.parse(data));
+
+  return {
+    props: {
+      weatherReport,
+    },
+  }
+}
+
 
 
 export default WeatherPage
